40785abf4c5c12ab94ac8b85f2cc9bce43f4ab77
Wesley Lins <wesleylins.soares@gmail.com>
2023-05-29 09:57:08 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState, useRef } from "react";
+import { View } from "react-native";
+import MapView, { Marker } from "react-native-maps";
+import {
+  requestForegroundPermissionsAsync,
+  getCurrentPositionAsync,
+  LocationObject,
+  watchPositionAsync,
+  LocationAccuracy,
+} from "expo-location";
+
+import { styles } from "./styles";
 
 export default function App() {
+  const [locations, setLocations] = useState<Array<LocationObject>>([]);
+  const mapRef = useRef<MapView>(null);
+
+  async function requestLocationPermissions() {
+    const { granted } = await requestForegroundPermissionsAsync();
+
+    if (granted) {
+      const currentPosition = await getCurrentPositionAsync();
+      setLocations([...locations, currentPosition]);
+      mapRef.current?.setCamera({
+        zoom: 16,
+      });
+    }
+  }
+
+  useEffect(() => {
+    requestLocationPermissions().catch((error) => console.log(error));
+  }, []);
+
+  useEffect(() => {
+    watchPositionAsync(
+      {
+        accuracy: LocationAccuracy.Highest,
+        timeInterval: 4000,
+        distanceInterval: 4,
+      },
+      (response) => {
+        setLocations([...locations, response]);
+        console.log("Location Nova!!");
+        mapRef.current?.animateCamera({
+          center: response.coords,
+        });
+      }
+    );
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {locations && (
+        <MapView ref={mapRef} style={styles.map}>
+          {locations.map((location, index) => (
+            <Marker key={index} coordinate={location.coords} />
+          ))}
+        </MapView>
+      )}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
